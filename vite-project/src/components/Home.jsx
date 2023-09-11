30a9495e7c7d74294f9734682fd0248c5bbb1f4b
Oussama Belkadi <oussama.belkadi72@gmail.com>
2023-09-11 14:39:08 +0200

    Grid pour l'accueil
    Ajout d'une grid pour l'accueil afin de bien disposer les éléments

--- vite-project/src/components/Home.jsx
+++ vite-project/src/components/Home.jsx
@@ -1,5 +1,27 @@
+import React from "react";
+import "../assets/style/Home.css"; 
+import { Link } from "react-router-dom";
+
+
 function Home() {
-    return <h1>Accueil</h1>;
-  }
-  
-  export default Home;
+  return (
+    <div className="parent">
+      <div className="div1">
+        <h2>AGENCE CRÉATION DE SITE WEB</h2>
+        <p>
+          Bienvenue dans notre agence web spécialisée dans la création de sites web
+          modernes et performants. Nous transformons vos idées en sites web
+          exceptionnels.
+        </p>
+        <Link to="/prices-and-quote">
+          <button>Découvrez nos tarifs</button>
+        </Link>
+      </div>
+      <div className="div2"></div>
+      <div className="div3">Contenu de div3</div>
+      <div className="div4">Contenu de div4</div>
+    </div>
+  );
+}
+
+export default Home;
