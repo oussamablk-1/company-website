9bdfa3285c279a1c91787c7470b98255bcf58136
Oussama Belkadi <oussama.belkadi72@gmail.com>
2023-09-12 13:29:13 +0200

    timeline accueil
    mise en place d'une timeline sur l'accueil pour les arguments

--- vite-project/src/components/Home.jsx
+++ vite-project/src/components/Home.jsx
@@ -1,25 +1,67 @@
 import React from "react";
 import "../assets/style/Home.css"; 
 import { Link } from "react-router-dom";
+import cercle from "/cercle.png"
 
 
 function Home() {
+
   return (
-    <div className="parent">
-      <div className="div1">
-        <h2>AGENCE CRÉATION DE SITE WEB</h2>
-        <p>
-          Bienvenue dans notre agence web spécialisée dans la création de sites web
-          modernes et performants. Nous transformons vos idées en sites web
-          exceptionnels.
-        </p>
-        <Link to="/prices-and-quote">
-          <button>Découvrez nos tarifs</button>
-        </Link>
+    <div>
+      <div className="parent">
+        <div className="div1">
+          <h2>AGENCE CRÉATION DE SITE WEB</h2>
+          <p>
+            Bienvenue dans notre agence web spécialisée dans la création de sites web
+            modernes et performants. Nous transformons vos idées en sites web
+            exceptionnels.
+          </p>
+          <Link to="/prices-and-quote">
+            <button>Découvrez nos tarifs</button>
+          </Link>
+        </div>
+        <div className="div2"></div>
       </div>
-      <div className="div2"></div>
-      <div className="div3">Contenu de div3</div>
-      <div className="div4">Contenu de div4</div>
+
+      <div className="timeline" >
+
+        <div className="container left-container">
+        <img src={cercle} alt="cercle" />
+          <div className="text-box">
+            <h2>Argument 1</h2>
+            <big>Chiffre</big>
+            <p>explication</p>
+          </div>
+        </div>
+
+        <div className="container right-container">
+        <img src={cercle} alt="cercle" />
+          <div className="text-box">
+            <h2>Argument 2</h2>
+            <big>Chiffre</big>
+            <p>explication</p>
+          </div>
+        </div>
+
+        <div className="container left-container">
+        <img src={cercle} alt="cercle" />
+          <div className="text-box">
+            <h2>Argument 3</h2>
+            <big>Chiffre</big>
+            <p>explication</p>
+          </div>
+        </div>
+
+        <div className="container right-container">
+        <img src={cercle} alt="cercle" />
+          <div className="text-box">
+            <h2>Argument 4</h2>
+            <big>Chiffre</big>
+            <p>explication</p>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
